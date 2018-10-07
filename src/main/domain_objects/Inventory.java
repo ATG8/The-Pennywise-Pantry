75d18c952c25c126c9e6c12586e269731aa3cfe2
Michelle Linn John <michelle.linn.john@gmail.com>
2018-10-07 12:34:26 -0400

    michelle-03 adding logic to read inventory and recipes from files; save commit, not done yet

--- src/main/domain_objects/Inventory.java
+++ src/main/domain_objects/Inventory.java
@@ -6,11 +6,20 @@
  */
 package main.domain_objects;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Instant;
-/**
-*  
-*/
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.stream;
+
+/**
+* Class to hold an Inventory item.
+*/
 public class Inventory {
    private long inventoryNumber;
    private String itemName;
@@ -18,7 +27,15 @@ public class Inventory {
    private Instant expireDate;
   
    public Inventory() {}
-  
+
+   /**
+    * Constructor.
+    *
+    * @param inventoryNumber the primary key
+    * @param itemName the name of the inventory item
+    * @param dateEntered the date the item was entered into the file
+    * @param expireDate the date the item expires
+    */
    public Inventory(long inventoryNumber, String itemName, Instant dateEntered, Instant expireDate) {
       this.inventoryNumber = inventoryNumber;
       this.itemName = itemName;
@@ -26,35 +43,85 @@ public class Inventory {
       this.expireDate = expireDate;
    }
 
+   /**
+    * @param inventoryNumber the primary key
+    */
    public void setInventoryNumber(long inventoryNumber) {
       this.inventoryNumber = inventoryNumber;
    }
-   
+
+   /**
+    * @return the primary key
+    */
    public long getInventoryNumber() {
       return inventoryNumber;
    }
 
+   /**
+    * @param itemName the name of the inventory item
+    */
    public void setItemName(String itemName) {
       this.itemName = itemName;
    }
-   
+
+   /**
+    * @return the name of the inventory item
+    */
    public String getItemName() {
       return itemName;
    }
 
+   /**
+    * @param dateEntered the date the item was entered
+    */
    public void setDateEntered(Instant dateEntered) {
       this.dateEntered = dateEntered;
    }
-   
+
+   /**
+    * @return the date the item was entered
+    */
    public Instant getDateEntered() {
       return dateEntered;
    }
 
+   /**
+    * @param expireDate the date the item expires
+    */
    public void setExpireDate(Instant expireDate) {
       this.expireDate = expireDate;
    }
-   
+
+   /**
+    * @return the date the item expires
+    */
    public Instant getExpireDate() {
       return expireDate;
+   }
+
+   /**
+    * Method to read the inventory file to create a {@link List} of Inventory items.
+    *
+    * @return a {@link List} of Inventory items
+    */
+   public static List<Inventory> getInventoryFromFile() {
+      List<Inventory> inventoryList = new ArrayList<>();
+      if (Files.exists(Paths.get("Inventory.txt"))) {
+         try {
+            List<String> contents = Files.readAllLines(Paths.get("Inventory.txt"));
+            contents.forEach(line -> {
+               List<String> lineContents = stream(line.split("\\|")).collect(Collectors.toList());
+               long itemNumber = Long.parseLong(lineContents.get(0));
+               long expiryDays = Long.parseLong(lineContents.get(3));
+               Instant dateEntered = Instant.parse(lineContents.get(4));
+               Instant expiryDate = dateEntered.plus(expiryDays, ChronoUnit.DAYS);
+               inventoryList.add(new Inventory(itemNumber, lineContents.get(1), dateEntered, expiryDate));
+            });
+
+         } catch (IOException ex) {
+            ex.printStackTrace();
+         }
+      }
+      return inventoryList;
    }
 }
--- src/main/domain_objects/Recipe.java
+++ src/main/domain_objects/Recipe.java
@@ -6,56 +6,112 @@
  */
 package main.domain_objects;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
 /**
-*  
-*/
+ *
+ */
 
 public class Recipe {
-   private long recipeNumber;
-   private String recipeName;
-   private List<String> ingredients;
-   private String instructions;
-  
-   public Recipe() {}
-  
-   public Recipe(long recipeNumber, String recipeName, List<String> ingredients, String instructions) {
-      this.recipeNumber = recipeNumber;
-      this.recipeName = recipeName;
-      this.ingredients = ingredients;
-      this.instructions = instructions;
-   }  
-   
-   public void setRecipeNumber(long recipeNumber) {
-      this.recipeNumber = recipeNumber;
-   }
-   
-   public long getRecipeNumber() {
-      return recipeNumber;
-   }
+  private String recipeName;
+  private List<IngredientAndQuantity> ingredients;
+  private String instructions;
 
-   public void setRecipeName(String recipeName) {
-      this.recipeName = recipeName;
-   }
-   
-   public String getRecipeName() {
-      return recipeName;
-   }
+  public Recipe() {}
 
-   public void setIngredients(List<String> ingredients) {
-      this.ingredients = ingredients;
-   }
-   
-   public List<String> getIngredients() {
-      return ingredients;
-   }
+  public Recipe(String recipeName, List<IngredientAndQuantity> ingredients, String instructions) {
+    this.recipeName = recipeName;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+  }
 
-   public void setInstructions(String instructions) {
-      this.instructions = instructions;
-   }
-   
-   public String getInstructions() {
-      return instructions;
-   }
-   
+  public void setRecipeName(String recipeName) {
+    this.recipeName = recipeName;
+  }
+
+  public String getRecipeName() {
+    return recipeName;
+  }
+
+  public void setIngredients(List<IngredientAndQuantity> ingredients) {
+    this.ingredients = ingredients;
+  }
+
+  public List<IngredientAndQuantity> getIngredients() {
+    return ingredients;
+  }
+
+  public void setInstructions(String instructions) {
+    this.instructions = instructions;
+  }
+
+  public String getInstructions() {
+    return instructions;
+  }
+
+  public List<Recipe> getRecipesFromFile() {
+    List<Recipe> recipeList = new ArrayList<>();
+    if (Files.exists(Paths.get("Recipes.txt"))) {
+      try {
+        List<String> contents = Files.readAllLines(Paths.get("Recipes.txt"));
+        contents.forEach(line -> {
+          List<String> lineContents = stream(line.split("\\|")).collect(Collectors.toList());
+          List<IngredientAndQuantity> ingredients = new ArrayList<>();
+          for (int i = 1; i < lineContents.size(); i++) {
+            String ingredient = lineContents.get(i);
+            ingredients.add(
+                new IngredientAndQuantity(ingredient.substring(0, ingredient.indexOf(",")),
+                    Integer.parseInt(ingredient.substring(ingredient.indexOf(",") + 1))));
+          }
+          String name = lineContents.get(0);
+          recipeList.add(new Recipe(name, ingredients, null));
+        });
+
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return recipeList;
+  }
+
+  private class IngredientAndQuantity {
+    private String ingredient;
+    private int quantity;
+
+    IngredientAndQuantity(String ingredient, int quantity) {
+      this.ingredient = ingredient;
+      this.quantity = quantity;
+    }
+
+    public String getIngredient() {
+      return ingredient;
+    }
+
+    public int getQuantity() {
+      return quantity;
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Recipe name: ")
+        .append(recipeName);
+    ingredients.forEach(ingredient -> sb.append("\nIngredient: ")
+        .append(ingredient.getIngredient())
+        .append("\nQuantity: ")
+        .append(ingredient.getQuantity()));
+    sb.append("\nInstructions: ")
+        .append(instructions);
+
+    return sb.toString();
+  }
+
 }
